8120cb52368a6340457879d2f55850ddd9a9c874
alupoaeiandreea <andreeaalupoaei@yahoo.com>
2017-08-11 15:39:31 +0300

    andreea - front end -->> service, companies, dashboard, login pages

--- booking/src/main/resources/templates/src/router/index.js
+++ booking/src/main/resources/templates/src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import Register from '@/components/Register'
 import Booking from '@/components/Booking'
 import Calendar from '@/components/Calendar'
+import LogIn from '@/components/LogIn'
+import Dashboard from '@/components/Dashboard'
+import AddServ from '@/components/AddServ'
+import Companies from '@/components/Companies'
+
 
 
 Vue.use(Router)
@@ -23,6 +28,23 @@ export default new Router({
       path: '/calendar',
       name: 'Calendar',
       component: Calendar
+    },
+     {
+      path: '/logIn',
+      name: 'LogIn',
+      component: LogIn
+    },{
+      path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard 
+    },{
+      path: '/addServ',
+        name: 'AddServ',
+        component: AddServ 
+    },{
+      path: '/companies',
+        name: 'Companies',
+        component: Companies 
     }
   ],
     mode: 'history'
