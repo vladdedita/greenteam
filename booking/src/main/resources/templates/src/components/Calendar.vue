7842c193b787b6e077999785c675d7590ea98624
rosu stefan <rosu.stefan96@gmail.com>
2017-08-15 13:45:42 +0300

    small identations fix

--- booking/src/main/resources/templates/src/components/Calendar.vue
+++ booking/src/main/resources/templates/src/components/Calendar.vue
@@ -2,29 +2,29 @@
   <div class="calendar">
    <navigation></navigation>
    <router-view></router-view>  
-    <div class="calendar-nav">
-        <span class="left-btn"><icon name="angle-left" aria-hidden="true" scale="3.5"></icon>
-          <h6>Prev.week</h6></span>
-        <p>Date</p>
-        <span><icon name="angle-right" aria-hidden="true" scale="3.5"></icon>
-          <h6>Next.week</h6></span>
+   <div class="calendar-nav">
+    <span class="left-btn"><icon name="angle-left" aria-hidden="true" scale="3.5"></icon>
+      <h6>Prev.week</h6></span>
+      <p>Date</p>
+      <span><icon name="angle-right" aria-hidden="true" scale="3.5"></icon>
+        <h6>Next.week</h6></span>
+      </div>
     </div>
-    </div>
-</template>
+  </template>
 
-<script>
-import navigation from '@/components/navigation'
-import 'vue-awesome/icons/angle-right'
-import 'vue-awesome/icons/angle-left'
-import Icon from 'vue-awesome/components/Icon'
-    export default {
-      name: 'calendar',
-      components: {
+  <script>
+  import navigation from '@/components/navigation'
+  import 'vue-awesome/icons/angle-right'
+  import 'vue-awesome/icons/angle-left'
+  import Icon from 'vue-awesome/components/Icon'
+  export default {
+    name: 'calendar',
+    components: {
       navigation, Icon
- }
     }
-</script>
-<style scoped>
+  }
+  </script>
+  <style scoped>
   .calendar-nav{
     display: inline-flex;
     margin-top: 60px;
@@ -49,5 +49,5 @@ import Icon from 'vue-awesome/components/Icon'
   .fa-icon {
     display: inline-block;
     fill: #818181;
-}
-</style>
+  }
+  </style>
